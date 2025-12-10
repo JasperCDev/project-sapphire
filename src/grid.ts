ef547f0b3157508711ce53afe963d18b69ecbebd
JasperCDev <jasperischauvin@gmail.com>
2025-12-09 22:42:49 -0600

    move canvas size logic to main

--- src/grid.ts
+++ src/grid.ts
@@ -1,30 +1,4 @@
 export class Grid {
   static ROW_COUNT = 18;
   static COL_COUNT = 32;
-  gridWidth: number = 0;
-  gridHeight: number = 0;
-  tileSize: number = 0;
-  canvas: HTMLCanvasElement;
-
-  constructor(canvas: HTMLCanvasElement) {
-    this.canvas = canvas;
-    this.init();
-  }
-
-  private init() {
-    window.addEventListener("resize", () => this.setGridSize());
-    this.setGridSize();
-  }
-
-  private setGridSize() {
-    let widthPercent = window.innerWidth / 16000000;
-    let heightPercent = window.innerHeight / 9000000;
-    let smallestPercent = Math.min(widthPercent, heightPercent);
-    this.gridWidth = Math.round(16000000 * smallestPercent);
-    this.gridHeight = Math.round(9000000 * smallestPercent);
-    this.tileSize = this.gridHeight / Grid.ROW_COUNT;
-
-    this.canvas.width = this.gridWidth;
-    this.canvas.height = this.gridHeight;
-  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,31 @@ import { Grid } from "./grid";
 import "./style.css";
 
 class Main {
-  grid: Grid;
+  grid = new Grid();
+  canvasWidth: number = 0;
+  canvasHeight: number = 0;
+  tileSize: number = 0;
+  canvas: HTMLCanvasElement;
+  context: CanvasRenderingContext2D;
 
   constructor() {
-    const c = document.getElementById("myCanvas") as HTMLCanvasElement;
-    const ctx = c.getContext("2d")!;
-    this.grid = new Grid(c);
+    this.canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+    this.context = this.canvas.getContext("2d")!;
+    window.addEventListener("resize", () => this.setCanvasSize());
+    this.setCanvasSize();
+    this.setCanvasSize();
+  }
+
+  private setCanvasSize() {
+    let widthPercent = window.innerWidth / 16000000;
+    let heightPercent = window.innerHeight / 9000000;
+    let smallestPercent = Math.min(widthPercent, heightPercent);
+    this.canvasWidth = Math.round(16000000 * smallestPercent);
+    this.canvasHeight = Math.round(9000000 * smallestPercent);
+    this.tileSize = this.canvasHeight / Grid.ROW_COUNT;
+
+    this.canvas.width = this.canvasWidth;
+    this.canvas.height = this.canvasHeight;
   }
 }
 
