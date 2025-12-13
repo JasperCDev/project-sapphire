7d01128f9c3493a1e1c06396cefadce915f12ea1
JasperCDev <jasperischauvin@gmail.com>
2025-12-12 23:33:09 -0600

    tile class

--- src/grid.ts
+++ src/grid.ts
@@ -1,20 +1,18 @@
 import { Entity } from "./entity";
+import { Tile } from "./tile";
 
 export class Grid extends Entity {
   static ROW_COUNT = 18;
   static COL_COUNT = 32;
+  private tiles = Array.from(
+    { length: Grid.COL_COUNT * Grid.ROW_COUNT },
+    (_, i) => new Tile(i)
+  );
 
   public draw(context: CanvasRenderingContext2D, dt: number): void {
     context.fillStyle = "#e0e0e0";
-    for (let row = 0; row < Grid.ROW_COUNT; row++) {
-      for (let col = 0; col < Grid.COL_COUNT; col++) {
-        context.fillRect(
-          col * (context.canvas.height / Grid.ROW_COUNT),
-          row * (context.canvas.height / Grid.ROW_COUNT),
-          context.canvas.height / Grid.ROW_COUNT -5,
-          context.canvas.height / Grid.ROW_COUNT -5
-        );
-      }
+    for (let tile of this.tiles) {
+      tile.draw(context, dt);
     }
   }
 }
---
+++ src/tile.ts
@@ -0,0 +1,56 @@
+import { Entity } from "./entity";
+import { Grid } from "./grid";
+
+class Point {
+  constructor(public row: number, public col: number) {}
+}
+
+export class Tile extends Entity {
+  public row: number;
+  public col: number;
+  public id: string;
+  static idHelper = 0;
+  constructor(tileIndex: number) {
+    super();
+    let tilePoint = Tile.getPointFromIterator(tileIndex);
+    this.id = Tile.getIdFromPoint(tilePoint);
+    this.row = tilePoint.row;
+    this.col = tilePoint.col;
+  }
+
+  draw(ctx: CanvasRenderingContext2D, deltaTime: number): void {
+    ctx.fillRect(
+      this.col * (ctx.canvas.height / Grid.ROW_COUNT),
+      this.row * (ctx.canvas.height / Grid.ROW_COUNT),
+      ctx.canvas.height / Grid.ROW_COUNT - 5,
+      ctx.canvas.height / Grid.ROW_COUNT - 5
+    );
+  }
+
+  static getIteratorFromId(id: string) {
+    let split = id.split("-");
+    let rowCount = parseInt(split[0]);
+    let colCount = parseInt(split[1]);
+    return Grid.COL_COUNT * (rowCount - 1) + colCount - 1;
+  }
+
+  static getIteratorFromPoint(point: Point) {
+    return (point.row - 1) * Grid.COL_COUNT + point.col - 1;
+  }
+
+  static getIdFromPoint(point: Point) {
+    Tile.idHelper++;
+    return (
+      point.row.toString().padStart(2, "0") +
+      "-" +
+      point.col.toString().padStart(2, "0")
+    );
+  }
+
+  static getPointFromIterator(i: number) {
+    let n = i + 1;
+    let row = Math.ceil(n / Grid.COL_COUNT);
+    let col = n % Grid.COL_COUNT || Grid.COL_COUNT;
+    return { row, col };
+  }
+}
